edaff7ac27521acb7a9ad01034d511f833c2e24d
Nathan Leavitt <nate@infusedsystems.com>
2012-02-01 22:47:11 -0700

    fixed rmv install

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,13 @@
 cd ~/
-bash < <( curl -s https://rvm.beginrescueend.com/install/rvm )
 for i in ~/.vim ~/.vimrc ~/.gvimrc ~/.bashrc ~/.bash_profile; do [ -e $i ] && mv $i $i.old; done
+bash -s stable < <(curl -s https://rvm.beginrescueend.com/install/rvm)
 # git clone git://github.com/carlhuda/janus.git ~/.vim
 ln -s ~/devenviro/vim/gvimrc ~/.gvimrc
 ln -s ~/devenviro/vim/vimrc ~/.vimrc
 ln -s ~/devenviro/bash/bashrc ~/.bashrc
 ln -s ~/devenviro/bash/bash_profile ~/.bash_profile
 ln -s ~/devenviro/git/gitconfig ~/.gitconfig
+mkdir ~/.ssh
 ln -s ~/devenviro/ssh/config ~/.ssh/config
 source ~/.bashrc
 source ~/.bash_profile
