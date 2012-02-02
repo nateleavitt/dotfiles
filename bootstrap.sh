023316f1e70c39e2eeddb147d1c47afee3891ca1
Nathan Leavitt <nate@infusedsystems.com>
2012-02-01 22:52:55 -0700

    added correct path to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 cd ~/
 for i in ~/.vim ~/.vimrc ~/.gvimrc ~/.bashrc ~/.bash_profile; do [ -e $i ] && mv $i $i.old; done
-bash -s stable < <(curl -s https://rvm.beginrescueend.com/install/rvm)
+bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
 # git clone git://github.com/carlhuda/janus.git ~/.vim
 ln -s ~/devenviro/vim/gvimrc ~/.gvimrc
 ln -s ~/devenviro/vim/vimrc ~/.vimrc
