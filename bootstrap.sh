a6d5cac61a011ec44b56e1e02c2c0be8435d719b
Nathan Leavitt <nate@infusedsystems.com>
2012-02-02 11:02:18 -0700

    added vim shortcut

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 cd ~/
 for i in ~/.vim ~/.vimrc ~/.gvimrc ~/.bashrc ~/.bash_profile; do [ -e $i ] && mv $i $i.old; done
-bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
 # git clone git://github.com/carlhuda/janus.git ~/.vim
+ln -s ~/devenviro/vim ~/.vim
 ln -s ~/devenviro/vim/gvimrc ~/.gvimrc
 ln -s ~/devenviro/vim/vimrc ~/.vimrc
 ln -s ~/devenviro/bash/bashrc ~/.bashrc
@@ -9,5 +9,6 @@ ln -s ~/devenviro/bash/bash_profile ~/.bash_profile
 ln -s ~/devenviro/git/gitconfig ~/.gitconfig
 mkdir ~/.ssh
 ln -s ~/devenviro/ssh/config ~/.ssh/config
+bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
 source ~/.bashrc
 source ~/.bash_profile
