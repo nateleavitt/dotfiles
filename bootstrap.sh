aa3b34745eb5ace8121f85e0a6fef31c4b1f952f
Nathan Leavitt <nate@infusedsystems.com>
2012-02-01 21:41:32 -0700

    added rvm install

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,5 @@
+cd ~/
+bash < <( curl -s https://rvm.beginrescueend.com/install/rvm )
 for i in ~/.vim ~/.vimrc ~/.gvimrc ~/.bashrc ~/.bash_profile; do [ -e $i ] && mv $i $i.old; done
 # git clone git://github.com/carlhuda/janus.git ~/.vim
 ln -s ~/devenviro/vim/gvimrc ~/.gvimrc
