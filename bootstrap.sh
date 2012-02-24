960e989c09eb509a3ba5eb68d9db90d4bf211d38
Nathan Leavitt <nate@infusedsystems.com>
2012-02-24 13:07:35 -0700

    updating to use janus config files

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,10 @@ function die()
     exit 1
 }
 
+# move devenviro
+echo 'moving devenviro to hidden'
+mv $HOME/devenviro ~/.devenviro
+
 # Add <strong>.old</strong> to any existing Vim file in the home directory
 for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc ~/.bashrc ~/.bash_profile; do
   if [[ ( -e $i ) || ( -h $i ) ]]; then
@@ -13,21 +17,22 @@ for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc ~/.bashrc ~/.bash_profile; do
 done
 
 # Adding bash environment files
-echo "creating bash environment files"
-ln -s $HOME/devenviro/bash/bashrc $HOME/.bashrc
-ln -s $HOME/devenviro/bash/bash_profile $HOME/.bash_profile
-ln -s $HOME/devenviro/git/gitconfig $HOME/.gitconfig
+ echo "creating bash environment files"
+ ln -s $HOME/.devenviro/bash/bashrc $HOME/.bashrc
+ ln -s $HOME/.devenviro/bash/bash_profile $HOME/.bash_profile
+ ln -s $HOME/.devenviro/git/gitconfig $HOME/.gitconfig
 
-echo "configuring ssh environment"
-mkdir $HOME/.ssh
-ln -s $HOME/devenviro/ssh/config $HOME/.ssh/config
+# echo "configuring ssh environment"
+ mkdir $HOME/.ssh
+ ln -s $HOME/.devenviro/ssh/config $HOME/.ssh/config
+ ln -s $HOME/.devenviro/janus $HOME/.janus
 
-echo "installing rvm"
-bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
+ echo "installing rvm"
+ bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
 
-echo "source bash files - you may need to do it manually"
-source $HOME/.bashrc
-source $HOME/.bash_profile
+ echo "source bash files - you may need to do it manually"
+ source $HOME/.bashrc
+ source $HOME/.bash_profile
 
 # Clone Janus into .vim
 git clone https://github.com/carlhuda/janus.git $HOME/.vim \
@@ -37,3 +42,10 @@ git clone https://github.com/carlhuda/janus.git $HOME/.vim \
 cd $HOME/.vim || die "Could not go into the ${HOME}/.vim"
 rake || die "Rake failed."
 
+# Remove and replace .vimrc.before after files
+echo "replacing default gvimrc and vimrc files installed by janus"
+rm $HOME/.gvimrc.before $HOME/.gvimrc.after $HOME/.vimrc.before $HOME/.vimrc.after
+ln -s $HOME/.devenviro/janus/gvimrc.before $HOME/.gvimrc.before
+ln -s $HOME/.devenviro/janus/gvimrc.after $HOME/.gvimrc.after
+ln -s $HOME/.devenviro/janus/vimrc.before $HOME/.vimrc.before
+ln -s $HOME/.devenviro/janus/vimrc.after $HOME/.vimrc.after
