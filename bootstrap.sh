b8f7d5b9774db7434dc9a5ba8d4c2a59b6b675e7
Nathan Leavitt <nate@spectre.local>
2014-08-27 23:27:32 -0700

    moving to dotfiles and removing hub

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,9 +4,9 @@ function die()
     exit 1
 }
 
-# move devenviro
-echo 'moving devenviro to hidden'
-mv $HOME/devenviro ~/.devenviro
+# move dotfiles
+echo 'moving dotfiles to hidden'
+mv $HOME/dotfiles ~/.dotfiles
 
 # Add <strong>.old</strong> to any existing Vim file in the home directory
 for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc ~/.bashrc ~/.bash_profile; do
@@ -18,15 +18,15 @@ done
 
 # Adding bash environment files
  echo "creating bash environment files"
- ln -s $HOME/.devenviro/bash/bashrc $HOME/.bashrc
- ln -s $HOME/.devenviro/bash/bash_profile $HOME/.bash_profile
- ln -s $HOME/.devenviro/git/gitconfig $HOME/.gitconfig
- ln -s $HOME/.devenviro/bash/git-completion.bash $HOME/.git-completion.bash
+ ln -s $HOME/.dotfiles/bash/bashrc $HOME/.bashrc
+ ln -s $HOME/.dotfiles/bash/bash_profile $HOME/.bash_profile
+ ln -s $HOME/.dotfiles/git/gitconfig $HOME/.gitconfig
+ ln -s $HOME/.dotfiles/bash/git-completion.bash $HOME/.git-completion.bash
 
  echo "configuring ssh environment"
  mkdir $HOME/.ssh
- ln -s $HOME/.devenviro/ssh/config $HOME/.ssh/config
-# ln -s $HOME/.devenviro/janus $HOME/.janus
+ ln -s $HOME/.dotfiles/ssh/config $HOME/.ssh/config
+# ln -s $HOME/.dotfiles/janus $HOME/.janus
 
  echo "installing rvm stable"
  # bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
@@ -48,12 +48,12 @@ done
 # Remove and replace .vimrc.before after files
 echo "replacing default gvimrc and vimrc files if installed"
 rm $HOME/.gvimrc.before $HOME/.gvimrc.after $HOME/.vimrc.before $HOME/.vimrc.after
-ln -s $HOME/.devenviro/vim $HOME/.vim
-ln -s $HOME/.devenviro/vim/gvimrc.before $HOME/.gvimrc.before
-ln -s $HOME/.devenviro/vim/gvimrc.after $HOME/.gvimrc.after
-ln -s $HOME/.devenviro/vim/vimrc.before $HOME/.vimrc.before
-ln -s $HOME/.devenviro/vim/vimrc.after $HOME/.vimrc.after
-ln -s $HOME/.devenviro/bin $HOME/bin
+ln -s $HOME/.dotfiles/vim $HOME/.vim
+ln -s $HOME/.dotfiles/vim/gvimrc.before $HOME/.gvimrc.before
+ln -s $HOME/.dotfiles/vim/gvimrc.after $HOME/.gvimrc.after
+ln -s $HOME/.dotfiles/vim/vimrc.before $HOME/.vimrc.before
+ln -s $HOME/.dotfiles/vim/vimrc.after $HOME/.vimrc.after
+ln -s $HOME/.dotfiles/bin $HOME/bin
 
 echo "installing Homebrew"
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
