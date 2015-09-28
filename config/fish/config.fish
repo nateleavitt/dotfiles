977aa6b2b82a6f890968f3b717b243b401e6b702
Nathan Leavitt <nateleavitt@gmail.com>
2015-09-28 15:05:02 -0700

    adding aliases to the config.fish file

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,3 +2,11 @@ rbenv init - | source
 
 set -x GOPATH $HOME/Code/go
 set -g -x PATH $PATH $GOPATH/bin
+
+# alias
+function u; cd ..; end
+function uu; cd ../..; end
+function uuu; cd ../../..; end
+
+function d; docker $argv; end
+function dm; docker-machine $argv; end
