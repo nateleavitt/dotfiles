dd4959e7e6c57fdbb6a6000b3def850b2b17e301
Nathan Leavitt <nateleavitt@gmail.com>
2017-01-24 11:51:34 -0700

    adding some docker-compose shortcuts

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,15 +7,20 @@ set -g -x PATH $PATH $GOPATH/bin
 function u; cd ..; end
 function uu; cd ../..; end
 function uuu; cd ../../..; end
+function typora; open -a typora $argv; end
 
-# function d; docker $argv; end
-function dm; docker-machine $argv; end
+function d; docker $argv; end
+# function dm; docker-machine $argv; end
 function c; cd $argv; end
 function t; terraform $argv; end
 
-function deval
-  eval (docker-machine env $argv)
-end
+function dcr; docker-compose run $argv; end
+function dcu; docker-compose up; end
+function dcd; docker-compose down; end
+function dcb; docker-compose build; end
 
-alias d docker
+# function deval
+#   eval (docker-machine env $argv)
+# end
 
+# alias d docker
