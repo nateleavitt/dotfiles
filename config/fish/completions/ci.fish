bb5165c111a66876f54ad2331343a872ab5f51b0
Nathan Leavitt <nateleavitt@gmail.com>
2018-09-30 00:39:16 -0700

    removing atom and fish config folders

--- config/fish/completions/ci.fish
+++
@@ -1,5 +0,0 @@
-function ci
-  if count $argv > /dev/null; cd $argv; else; cd ~/Code/github.com/infusionsoft/; end
-end
-
-complete --command ci --exclusive --arguments "(__fish_complete_directories (~/Code/github.com/infusionsoft/))"
--- config/fish/completions/cn.fish
+++
@@ -1,5 +0,0 @@
-function cn
-  if count $argv > /dev/null; cd $argv; else; cd ~/Code/github.com/nateleavitt/; end
-end
-
-complete --command cn --exclusive --arguments "(__fish_complete_directories (~/Code/github.com/nateleavitt/))"
