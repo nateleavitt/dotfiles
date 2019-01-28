d7824f3026ba8a317c7509c99c7cf7888836b938
Nathan Leavitt <nateleavitt@gmail.com>
2019-01-28 11:15:21 -0700

    Removing preview from vscode and installing search plugin for hyper

--- hyper/hyper.js
+++ hyper/hyper.js
@@ -131,7 +131,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyperterm-material-dark'],
+  plugins: ['hyperterm-material-dark', "hyper-search"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
