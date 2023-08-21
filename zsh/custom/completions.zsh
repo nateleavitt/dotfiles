d58d086e9f3165d1dafbe6633c2cb14394170967
nateleavitt <nateleavitt@gmail.com>
2023-08-21 09:36:39 -0700

    Updating bootstrap script and adding other shortcuts

--- zsh/custom/completions.zsh
+++ zsh/custom/completions.zsh
@@ -3,3 +3,6 @@ compctl -W ~/Code/github.com/nateleavitt/ -/ cn
 
 cl() { cd ~/Code/github.com/loyalstream/$1; }
 compctl -W ~/Code/github.com/loyalstream/ -/ cl
+
+cb() { cd ~/Code/github.com/bryanu/$1; }
+compctl -W ~/Code/github.com/bryanu/ -/ cb
