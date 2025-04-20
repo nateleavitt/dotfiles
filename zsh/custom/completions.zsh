d3e86130a58b9a5fdf2f1cfc962ed4f234128dd6
Nate Leavitt <nate@mk4.local>
2025-04-19 23:35:37 -0700

    Update dotfiles

--- zsh/custom/completions.zsh
+++ zsh/custom/completions.zsh
@@ -1,11 +1,8 @@
-cn8() { cd ~/Code/github.com/n8bit/$1; }
-compctl -W ~/Code/github.com/n8bit/ -/ cn8
+cn8() { cd ~/Developer/github.com/n8bit/$1; }
+compctl -W ~/Developer/github.com/n8bit/ -/ cn8
 
-cn() { cd ~/Code/github.com/nateleavitt/$1; }
-compctl -W ~/Code/github.com/nateleavitt/ -/ cn
+cn() { cd ~/Developer/github.com/nateleavitt/$1; }
+compctl -W ~/Developer/github.com/nateleavitt/ -/ cn
 
-cl() { cd ~/Code/github.com/loyalstream/$1; }
-compctl -W ~/Code/github.com/loyalstream/ -/ cl
-
-cb() { cd ~/Code/github.com/bryanu/$1; }
-compctl -W ~/Code/github.com/bryanu/ -/ cb
+cl() { cd ~/Developer/github.com/loyalstream/$1; }
+compctl -W ~/Developer/github.com/loyalstream/ -/ cl
